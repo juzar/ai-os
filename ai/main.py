9a4763fd8a83cb804e5c58ad3355c151816015e1
juzar <juzar@users.noreply.github.com>
2026-04-08 18:24:51 +0530

    update ai/main.py

--- ai/main.py
+++ ai/main.py
@@ -1,22 +1,21 @@
 import sys
 from ai.executor import run_model
+from ai.chat import chat_loop
+
 
 def main():
-    if len(sys.argv) < 3:
-        print("\nUsage: ai <mode> <query>\n")
-        print("Modes: devops | research | general\n")
+
+    # CLI MODE
+    if len(sys.argv) > 2:
+        mode = sys.argv[1]
+        query = " ".join(sys.argv[2:])
+
+        result = run_model(mode, query)
+        print(result)
         return
 
-    mode = sys.argv[1]
-    query = " ".join(sys.argv[2:])
-
-    print(f"\n⚡ Mode: {mode}")
-    print(f"🧠 Query: {query}\n")
-
-    result = run_model(mode, query)
-
-    print("\n🔥 RESULT:\n")
-    print(result)
+    # CHAT MODE
+    chat_loop()
 
 
 if __name__ == "__main__":
